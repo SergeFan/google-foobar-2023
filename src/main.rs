4e80135f1c8011693d0251339aeb9feb5fc11531
Jaeson Fan <sergefan@live.com>
2023-11-25 03:02:55 +0900

    minor adjustment

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use google_foobar_2023::lib::braille::convert_str_to_braille_code;
+
+fn main() {
+    // Challenge 1: Braille Translation
+    println!(
+        "Braille Code: {}",
+        convert_str_to_braille_code("Braille Code")
+    );
+}
